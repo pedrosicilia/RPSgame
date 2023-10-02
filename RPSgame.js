ebdf80c88cf60b815a5c4281bcabdae207e01b30
PedroSicilia <32110851+pedrosicilia@users.noreply.github.com>
2023-10-02 04:59:17 -0400

    Working Score Keeper and Button Selector

--- RPSgame.js
+++ RPSgame.js
@@ -1,5 +1,14 @@
-const container = document.querySelector('#container');
 
+let playerSelection = 'rock';
+let computerSelection;
+let compScore = 0;
+let playerScore = 0;
+
+
+
+const container = document.querySelector('#container');
+const indivresults = document.querySelector('#indivresults');
+const results = document.querySelector('#results');
 
 const rockbtn = document.createElement('button');
 rockbtn.textContent = "Rock";
@@ -14,23 +23,31 @@ scissorsbtn.id = 'scissors';
 container.appendChild(rockbtn);
 container.appendChild(paperbtn);
 container.appendChild(scissorsbtn);
-
+indivresults.textContent = `${playerScore} - ${compScore}`;
 
 const buttons = container.querySelectorAll('button')
 
 buttons.forEach((button) => {
 
   button.addEventListener('click', (i) => {
-      i.target.style.background = 'red';
+      playerSelection = button.id;
+      console.log(playRound(playerSelection, getComputerChoice()))
+      indivresults.textContent = `${playerScore} - ${compScore}`;
+  });
   
-  })
 });
 
 
-
+let addcompScore = () =>{
+  compScore++;
+  return compScore;
+};
+let addplayerScore = () =>{
+  playerScore++;
+  return playerScore;
+};
 
 let getComputerChoice = () =>  {
- 
   const rand = Math.floor(Math.random() *3);
   switch(rand){
     case 0:
@@ -55,27 +72,33 @@ function playRound(playerSelection, getComputerChoice) {
   console.log("User: ", playerSelection , "\nComputer : ", getComputerChoice)
 
   if(playerSelection == 'rock' && getComputerChoice == 'paper') {
+    addcompScore();
     return "You Lose!";
   }
   if(playerSelection == 'rock' && getComputerChoice == 'scissors') {
+    addplayerScore();
     return "You Win!";
   }
   if(playerSelection == 'rock' && getComputerChoice == 'rock') {
     return "It's a Tie!";
   }
   if(playerSelection == 'paper' && getComputerChoice == 'rock') {
+    addplayerScore();
     return "You Win!";
   }
   if(playerSelection == 'paper' && getComputerChoice == 'scissors') {
+    addcompScore();
     return "You Lose!";
   }
   if(playerSelection == 'paper' && getComputerChoice == 'paper') {
     return "It's a Tie!";
   }
   if(playerSelection == 'scissors' && getComputerChoice == 'paper') {
+    addplayerScore();
     return "You Win!";
   }
   if(playerSelection == 'scissors' && getComputerChoice == 'rock') {
+    addcompScore();
     return "You Lose!";
   }
   if(playerSelection == 'scissors' && getComputerChoice == 'scissors') {
@@ -95,7 +118,7 @@ let game = () => {
  // console.log("playerSelection: ", computerSelection);
 
   for(let i = 0; i < 5;i++) {
-    let result = playRound(playerChoice(), getComputerChoice());
+    let result = playRound(playerSelection, getComputerChoice());
     //console.log("RESULT: ", result);
     switch(result) {
       case 'You Win!':
@@ -134,9 +157,9 @@ let sum = function(a, b) {
   return a + b;
 };
 */
-const playerSelection = playerChoice();
-const computerSelection = getComputerChoice();
+//const playerSelection = playerChoice();
+//const computerSelection = getComputerChoice();
 
 
 
-console.log(game()); 
+//console.log(game()); 
